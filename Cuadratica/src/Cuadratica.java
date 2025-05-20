879b5f052178181105eb2ac8ea7cc7d97e33b444
Brayan Valencia <80861283+bsvalenciaq@users.noreply.github.com>
2025-05-20 07:50:17 -0500

    cambios en cuadratica

--- Cuadratica/src/Cuadratica.java
+++ Cuadratica/src/Cuadratica.java
@@ -3,24 +3,28 @@ import java.util.Scanner;
 public class Cuadratica {
     public static void main(String[] args) {
         //ax^2+bx+c
-        double a, b, c;
+        var scanner = new Scanner(System.in);
+        //captura datos
+        System.out.print("Ingrese el valor de a: ");
+        var a = scanner.nextDouble();
+        if (a != 0) {
+            System.out.print("Ingrese el valor de b: ");
+            var b = scanner.nextDouble();
+            System.out.print("Ingrese el valor de c: ");
+            var c = scanner.nextDouble();
+            var dis = Math.sqrt(Math.pow(b, 2) - 4 + a + c);
+            if (dis > 0) {
+                //cuadratica
+                var cuadraticaAritSuma = (-b + dis) / (2 * a);
+                var cuadraticaAritResta = (-b - dis) / (2 * a);
+                System.out.println("cuadratica resta = " + cuadraticaAritResta);
+                System.out.println("Cuadratica suma = " + cuadraticaAritSuma);
+            } else
+                System.out.println("El valor discriminante no puede ser negativo");
 
-        Scanner scanner = new Scanner(System.in);
+        } else
 
-        System.out.println("Ingrese el valor de a: ");
-        a = scanner.nextDouble();
-        System.out.println("Ingrese el valor de b: ");
-        b = scanner.nextDouble();
-        System.out.println("Ingrese el valor de c: ");
-        c = scanner.nextDouble();
-
-        //cuadratica
-        double cuadraticaAritSuma = (-b + (1 / (b * b - 4 * a * c)) / (2 * a));
-        double cuadraticaAritResta = (-b - (1 / (b * b - 4 * a * c)) / (2 * a));
-
-        System.out.println("cuadratica resta = " + cuadraticaAritResta);
-        System.out.println("Cuadratica suma = " + cuadraticaAritSuma);
-
+            System.out.println("El valor de a no puede ser cero");
 
     }
 }
